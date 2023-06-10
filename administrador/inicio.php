103b502f0366f535c7cf0d5b3eda894607e9f8e6
Eduardo2405 <tavio.jtx@gmail.com>
2023-06-10 12:08:32 -0600

    Fusionando ramas :)

--- administrador/inicio.php
+++ administrador/inicio.php
@@ -2,17 +2,31 @@
 
 <!--INICIO del cont principal-->
 <div class="container">
-    <h1>Contenido principal</h1>
+    <h1><img src='vendor/img/logo1.png' alt=''> Bienvenido a BioN4ture</h1>
+    
 
 <div class="container">
         <div class="row">
             <div class="col-lg-12">            
+                <p>
+                BioNature somos una micro empresa que te ofrece una variedad de productos para el cuidado
+                    de tu piel e higiene personal, nuestros jabones hechos de ingredientes 100% de origen natural 
+                    te ofrecen una sensacion de limpieza y frescura.
+                    En BioN4ture nos enorgullece brindarte los productos de la mejor calidada. 
+                    Explora nuestra colección y descubre una variedad de productos y formulas cuidadosamente seleccionadas para cada tipo de piel. 
+                    Desde jabones suaves y humectantes hasta exfoliantes revitalizantes, 
+                    tenemos el producto perfecto para cada necesidad.
+                </p>
+                <hr>
+            </div> 
+            <div class="col-lg-12">
+                <img src="vendor/img/jb5.jpg" alt="" >
                 
-            </div>    
+            </div> 
+            
         </div>    
     </div>    
     <br>  
-
-
+   
 <!--FIN del cont principal-->
 <?php require_once "vistas/page_down.php"?>

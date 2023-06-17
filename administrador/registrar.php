2eebdb956f8837a3609dfdbd7e21cdd72f6f7348
Eduardo2405 <tavio.jtx@gmail.com>
2023-06-17 12:01:40 -0600

    Agregue unos cambios

--- administrador/registrar.php
+++ administrador/registrar.php
@@ -1,5 +1,18 @@
-<?php require_once "vistas/page_top.php "?>
+<?php require_once "vistas/page_top.php"?>
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1><img src='vendor/img/logo1.png' alt=''> Bienvenido a BioN4ture</h1>
+    
+
+<div class="container"> <!-- contenido principal    INICIO   -->
+        <div class="row">
+            
+            
+        </div>    
+</div>                  <!-- contenido principal FIN -->
+    
+
+<!--FIN del cont principal-->
 
 
-
-<?php require_once "vistas/page_down.php "?>
+<?php require_once "vistas/page_down.php"?>
